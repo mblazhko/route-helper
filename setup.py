9e35d8221a5467e7c7fafdeaee9fd856fdc3eb99
Maks Blazhko <makssimm1@gmail.com>
2024-02-20 12:01:28 +0100

    remove redundant

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     package_dir={'': 'route_helper'},
     description="Help find routes avoiding Switzerland using Google Maps API",
     author="Maks Blazhko",
-    namespace_packages=['route_helper'],
     author_email="maksym.blazhko.pydev@gmail.com",
     url="https://github.com/mblazhko/route-helper",
     install_requires=("googlemaps>=4.10.0",),
